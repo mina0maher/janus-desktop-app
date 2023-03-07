4a93b980c2b6b250d5a203f1fcf7f630bf47a8a4
mina0maher <mahermena160@gmail.com>
2023-03-07 23:32:03 +0200

    handle getting gates

---
+++ src/main/java/models/GatesModel.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author maher
+ */
+public class GatesModel {
+        public int id;
+        public String name;
+        public Location location;
+        public String address;
+        public ArrayList<Price> prices;
+        public String imageUrl;
+}
---
+++ src/main/java/models/Price.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author maher
+ */
+public class Price{
+    public VehicleType vehicleType;
+    public double price;
+}
